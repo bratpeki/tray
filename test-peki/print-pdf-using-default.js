b11308ac65eb18b89a15ded5fdd41d91882316c4
bratpeki <pkatic2003@gmail.com>
2025-06-25 19:14:54 +0200

    There's gotta be a more clean approach to this JavaScript code...

--- test-peki/print-pdf-using-default.js
+++ test-peki/print-pdf-using-default.js
@@ -1,23 +1,40 @@
+// Get an OS-agnostic path from strings
+// Given how I'm using CUPS-PDF,
+//   as of now I'm assuming we're running these on Linux systems
+//   and I'll adjust it for Mac down the line.
 const path = require('path');
-const qz = require("../js/qz-tray")
 
-// Shorthand
+// List files in a directory
+const fs = require('fs');
+
+// Piece de resistance!
+const qz = require("../js/qz-tray");
+
+///////////////////////////////////////////////////////////////////////////
+
+// OS username
+const username = require("os").userInfo().username;
+
+const qzroot = path.join(__dirname, "..");
+const pdfSample = path.join(qzroot, "assets", "pdf_sample.pdf");
+
 function print(x) { console.log(x); }
+
+///////////////////////////////////////////////////////////////////////////
 
 qz.websocket.connect()
 
 	.then( () => { return qz.printers.find(); } )
-	.then( (found) => {
 
+	.then( (found) => {
 		print("Printer list:");
 		for ( var i = 0; i < found.length; i++ )
 			print("  " + found[i]);
-
 	} )
 
 	.then( () => { return qz.printers.getDefault(); } )
-	.then( (found) => {
 
+	.then( (found) => {
 		print("Set printer to default (" + found + ")");
 		var config = qz.configs.create(found);
 
@@ -27,17 +44,29 @@ qz.websocket.connect()
 			flavor: 'file',
 			// A bit of explanation is in order here.
 			// We're in the testing directory, we go back into the root, and the to the PDF sample to print it.
-			data: "file://" + path.join(__dirname, "..", "assets", "pdf_sample.pdf")
+			data: "file://" + pdfSample
 		}];
-		return qz.print(config, data).catch(function(e) { console.error(e); });
 
+		return qz.print(config, data).catch(function(e) { console.error(e); });
 	} )
 
-	.finally(() => {
+	.then( (x) => {
 		// Ensures disconnect happens even on error
-		qz.websocket.disconnect().then(() => {
-			console.log("Disconnected, exiting...");
-			process.exit(0);  // force exit if needed
-		});
-	});
+		qz.websocket.disconnect().then(async () => {
 
+			// I'm aware of how stupid this is, LMAO!
+			print("Waiting 3 seconds to make sure the PDF is there");
+			await new Promise(resolve => setTimeout(resolve, 3000));
+
+			pdfPath = path.join("/", "var", "spool", "cups-pdf", username);
+			pdfList = fs.readdirSync(pdfPath);
+			pdfTarget = pdfList[pdfList.length - 1];
+
+			print("Listing contents of " + pdfPath + ": " + pdfList);
+			print("Target: " + path.join(pdfPath, pdfTarget));
+
+			console.log("Exiting...");
+			process.exit(0);
+
+		} );
+	} )
