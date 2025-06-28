050c32f379201153fc919f3f6a3889441d6dab11
bratpeki <pkatic2003@gmail.com>
2025-06-28 19:22:55 +0200

    Refactored

--- test-peki/utils/pixelsComp.mjs
+++ test-peki/utils/pixelsComp.mjs
@@ -1,6 +1,10 @@
 
 import { pdf2pixels } from "./pdf2pixels.mjs";
+
 import pixelmatch from "pixelmatch";
+
+import { PNG } from "pngjs";
+import fs from "fs";
 
 export async function pixelsComp(path1, path2) {
 
@@ -14,9 +18,11 @@ export async function pixelsComp(path1, path2) {
 			throw new Error("Images have different dimensions");
 		}
 
+		// RGBA buffer, four bytes per pixel
 		const diffBuffer = new Uint8ClampedArray(img1.width * img1.height * 4);
 
-		// https://github.com/mapbox/pixelmatch?tab=readme-ov-file#api
+		// We know the return value based on:
+		// https://github.com/mapbox/pixelmatch/blob/main/index.js#L100
 		const numDiffPixels = pixelmatch(
 			img1.data,
 			img2.data,
@@ -25,6 +31,13 @@ export async function pixelsComp(path1, path2) {
 			img1.height,
 			{ threshold: 0.1 }
 		);
+
+		// If we want the output generated
+		//
+		// const {width, height} = img1;
+		// const diff = new PNG({width, height});
+		// diff.data = diffBuffer;
+		// fs.writeFileSync('diff.png', PNG.sync.write(diff));
 
 		return numDiffPixels === 0;
 
