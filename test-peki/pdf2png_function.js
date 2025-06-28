ba9ab2072a8d7c0b8fc874a64d9bd61a283e2ed8
bratpeki <pkatic2003@gmail.com>
2025-06-28 13:45:29 +0200

    pdf2png_function.js

---
+++ test-peki/pdf2png_function.js
@@ -0,0 +1,55 @@
+import fs from "fs";
+import path from "path";
+import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
+
+// External resource URLs
+const CMAP_URL = "../node_modules/pdfjs-dist/cmaps/";
+const CMAP_PACKED = true;
+const STANDARD_FONT_DATA_URL = "../node_modules/pdfjs-dist/standard_fonts/";
+
+// Main function to load PDF and convert first page to PNG
+export async function processPdfToImage(pdfPath, outputImagePath) {
+
+	try {
+
+		const data = new Uint8Array(fs.readFileSync(pdfPath));
+
+		const loadingTask = getDocument({
+			data,
+			cMapUrl: CMAP_URL,
+			cMapPacked: CMAP_PACKED,
+			standardFontDataUrl: STANDARD_FONT_DATA_URL,
+		});
+
+		const pdfDocument = await loadingTask.promise;
+		console.log("# PDF document loaded.");
+
+		const page = await pdfDocument.getPage(1);
+		const canvasFactory = pdfDocument.canvasFactory;
+		const viewport = page.getViewport({ scale: 1.0 });
+
+		const canvasAndContext = canvasFactory.create(
+			viewport.width,
+			viewport.height
+		);
+
+		const renderContext = {
+			canvasContext: canvasAndContext.context,
+			viewport,
+		};
+
+		const renderTask = page.render(renderContext);
+		await renderTask.promise;
+
+		const image = canvasAndContext.canvas.toBuffer("image/png");
+		fs.writeFileSync(outputImagePath, image);
+		console.log(`Image written to ${outputImagePath}`);
+
+		page.cleanup();
+
+	} catch (err) { console.error("Failed to process PDF:", err); }
+
+}
+
+await processPdfToImage("./assets/rot.pdf", "rot.png");
+await processPdfToImage("./assets/norot.pdf", "norot.png");
