d577cf14d5b1f2b2ecee53a7cfc9d7dda57c8ae8
bratpeki <pkatic2003@gmail.com>
2025-06-28 19:10:58 +0200

    Documented pdf2pixels.mjs

--- test-peki/utils/pdf2pixels.mjs
+++ test-peki/utils/pdf2pixels.mjs
@@ -1,3 +1,4 @@
+
 import fs from "fs";
 import path from "path";
 import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
@@ -7,14 +8,19 @@ const CMAP_URL = "../node_modules/pdfjs-dist/cmaps/";
 const CMAP_PACKED = true;
 const STANDARD_FONT_DATA_URL = "../node_modules/pdfjs-dist/standard_fonts/";
 
-// Main function to load PDF and convert first page to PNG
-// OLD: export async function pdf2png(pdfPath, outputImagePath)
+// Converts PDF files on 'pdfPath' to a pixel buffer
+// The output is a buffer array of size W*H*4, each pixel needs 4 bytes for RGBA values
+// So, the format of the output is [ R,G,B,A, R,G,B,A, R,G,B,A, ... ]
 export async function pdf2pixels(pdfPath) {
 
 	try {
 
+		// Raw PDF file data
 		const data = new Uint8Array(fs.readFileSync(pdfPath));
 
+		// https://mozilla.github.io/pdf.js/api/draft/module-pdfjsLib.html
+		//
+		// 'getDocument' returns an object of type 'PDFDocumentLoadingTask'
 		const loadingTask = getDocument({
 			data,
 			cMapUrl: CMAP_URL,
@@ -22,10 +28,24 @@ export async function pdf2pixels(pdfPath) {
 			standardFontDataUrl: STANDARD_FONT_DATA_URL,
 		});
 
+		// Once the promise of 'PDFDocumentLoadingTask' is resolved, it returns an object of type 'PDFDocumentProxy'
 		const pdfDocument = await loadingTask.promise;
 
+		// You can then run 'getPage' on that 'PDFDocumentProxy' object
 		const page = await pdfDocument.getPage(1);
+
+		// pdf.js provides a canvas factory for us
+		//
+		// $ grep NodeCanvasFactory -r .
+		// ./pdf.mjs:class NodeCanvasFactory extends BaseCanvasFactory {
+		// ./pdf.mjs:  const CanvasFactory = src.CanvasFactory || (isNodeJS ? NodeCanvasFactory : DOMCanvasFactory);
+		// ...
+		//
+		// A canvas factory is used to create and manage canvases in a platform-independent way.
+		// We basically only need it for the 'create' method, which
+		// returns a canvas and its 2D context, sized to the given width and height.
 		const canvasFactory = pdfDocument.canvasFactory;
+
 		const viewport = page.getViewport({ scale: 1.0 });
 
 		const canvasAndContext = canvasFactory.create(
@@ -51,17 +71,15 @@ export async function pdf2pixels(pdfPath) {
 		page.cleanup();
 
 		return {
-			data: imageData.data,
-			width: viewport.width,
-			height: viewport.height
+			data: imageData.data, // The RGBA pixels, exactly what we're here for!
+			width: viewport.width, // An int
+			height: viewport.height // Another int
 		};
 
 	} catch (err) {
 		console.error("Failed to process PDF:", err);
-		// throw err;
+		throw err;
 	}
 
 }
 
-// await pdf2png("../assets/rot.pdf", "rot.png");
-// await pdf2png("../assets/norot.pdf", "norot.png");
